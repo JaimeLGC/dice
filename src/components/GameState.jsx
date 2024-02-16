0b4803ea8cee73c04e98b847a43d7ce10d9856b0
jaimelgc <jaimeleongarcia123@gmail.com>
2024-02-16 13:15:10 +0000

    52

--- src/components/GameState.jsx
+++ src/components/GameState.jsx
@@ -1,9 +1,26 @@
+import TotalPoints from "./TotalPoints";
+import Selector from "./Selector";
+import styled from "styled-components";
+import Dice from "./Dice";
+
 const GameState = () => {
   return (
-    <main>
-
-    </main>
+    <MainContainer>
+      <div className="top">
+        <TotalPoints />
+        <Selector />
+      </div>
+      <Dice />
+    </MainContainer>
   );
 };
 
 export default GameState;
+
+const MainContainer = styled.main`
+  .top {
+   display: flex;
+   justify-content: space-between;
+    align-items: end;
+  }
+`;
